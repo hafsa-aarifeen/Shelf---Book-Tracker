e457ba42d5547a882395126696d3d7b4a0948d90
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2026-07-31 17:36:21 +0530

    update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
-    base: "/Shelf---Book-Tracker/",
+    base: "/Shelf/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
